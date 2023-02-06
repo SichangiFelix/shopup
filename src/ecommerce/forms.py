c11ce9e295d5e23ab2e0397eb650b5b9d01ec146
SichangiFelix <sichangifelix23@gmail.com>
2023-02-06 23:13:10 +0300

    Login and registration forms

--- src/ecommerce/forms.py
+++ src/ecommerce/forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class ContactForm(forms.Form):
     fullname = forms.CharField(
@@ -31,3 +34,41 @@ class ContactForm(forms.Form):
         if not "gmail.com" in email:
             raise forms.ValidationError("Email has to be gmail.com")
         return email
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(
+        widget= forms.PasswordInput()
+    )
+
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    email = forms.CharField()
+    password = forms.CharField(
+        widget = forms.PasswordInput()
+    )
+    password2 = forms.CharField(
+        widget = forms.PasswordInput(), label = "confirm password"
+    )
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        qs = User.objects.filter(username = username)
+        if qs.exists():
+            raise forms.ValidationError("Username already exsists")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        qs = User.objects.filter(email = email)
+        if qs.exists():
+            raise forms.ValidationError("email already exsists")
+        return email
+
+    def clean(self):
+        data = self.cleaned_data
+        password = self.cleaned_data.get("password")
+        password2 = self.cleaned_data.get("password2")
+        if password != password2:
+            raise forms.ValidationError("Passwords must match.")
+        return data
