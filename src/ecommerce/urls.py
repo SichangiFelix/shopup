3b5067d75db51e7effb15179b5be5f7af35e2a26
SichangiFelix <sichangifelix23@gmail.com>
2023-02-06 23:13:30 +0300

    login and register urls

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('home/', views.home_page),
     path('about/', views.about_page),
     path('contact/', views.contact_page),
+    path('login/', views.login_page),
+    path('register/', views.register_page),
 ]
